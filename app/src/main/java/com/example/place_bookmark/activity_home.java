a5ba88eb278fb2e844d8abc0deb619683141f4ae
Han <oiuy00@naver.com>
2021-03-31 22:52:41 +0900

    update main acitivity

---
+++ app/src/main/java/com/example/place_bookmark/activity_home.java
@@ -0,0 +1,66 @@
+package com.example.place_bookmark;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+public class activity_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item)
+    {
+        Toast toast = Toast.makeText(getApplicationContext(),"", Toast.LENGTH_LONG);
+
+        switch(item.getItemId())
+        {
+            /*
+            case R.id.menu1:
+                toast.setText("Select Menu1");
+                break;
+            case R.id.menu2:
+                toast.setText("Select Menu2");
+                break;
+            case R.id.menu3:
+                toast.setText("Select Menu3");
+                break;
+        */
+        }
+
+
+        toast.show();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
